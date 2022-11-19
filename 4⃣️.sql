e8a898cb55387b76d943bc6f844a6c67b7b43288
QiutingWang <96046280+QiutingWang@users.noreply.github.com>
2022-11-20 07:50:25 +0800

    Update 4⃣️.sql

--- 4⃣️.sql
+++ 4⃣️.sql
@@ -41,7 +41,7 @@ Table: UserVisits
 +-------------+------+
 This table does not have a primary key.
 This table contains logs of the dates that users visited a certain retailer.
-Assume today's date is '2021-1-1'.
+Assume today date is "2021-1-1".
 Write an SQL query that will, for each user_id, find out the largest window of days between each visit and the one right after it (or today if you are considering the last visit).
 Return the result table ordered by user_id.
 
