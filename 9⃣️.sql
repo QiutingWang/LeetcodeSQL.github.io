a615866855f023d80b0d98f7233020c80a5a5e53
QiutingWang <96046280+QiutingWang@users.noreply.github.com>
2023-08-12 11:23:05 +0800

    Update 9⃣️.sql

--- 9⃣️.sql
+++ 9⃣️.sql
@@ -42,3 +42,43 @@ SELECT ROUND(SUM(
 FROM Activity
 JOIN firstTime ON Activity.player_id=firstTime.player_id
 
+
+-- SWAP类
+-- LeetCode 627 swap by update
+UPDATE Salary
+SET sex= CASE sex
+         WHEN 'm' THEN 'f'
+         ELSE 'm'
+         END
+-- UPDATE: to modify the existing records in a table.(DML clause)
+  -- other DML clause: DELETE, INSERT
+-- syntax:
+UPDATE table_name
+SET column1 = value1, column2 = value2, ...
+WHERE conditions --by using WHEN clause, we can only change selected rows, not the whole dataset
+
+
+-- LeetCode 626 Swap every 2 consecutive students
+SELECT (CASE WHEN MOD(id,2) != 0 AND counts!=id THEN id+1 --if it is odd-th id, but not the last one
+             WHEN MOD(id,2) != 0 AND counts=id THEN id --if it is the last seat and odd-th id, fixed not change
+             ELSE id-1 --for even-th id
+        END) AS id,
+        student
+FROM Seat,
+     (SELECT COUNT(*) AS counts
+      FROM Seat) AS seat_counts
+ORDER BY id ASC
+-- MOD(value, divisor): same to `value%divisor`
+
+-- Another method:
+SELECT s1.id, COALESCE(s2.student, s1.student) AS student
+FROM Seat s1
+LEFT JOIN Seat s2 ON ((s1.id+1)^1)-1=s2.id
+ORDER BY s1.id
+-- COALESCE: return the first not NULL value
+-- Syntax: COALESCE(val1, val2, ...., val_n)
+-- ^ symbol: 正则表达式，在expression2范围之外的的字符;有时也是power of value
+  -- Syntax: expression1^expression2
+
+
+-- Other points: UNIQUE, PIVOT, CAST, CONVERT, EXTRACT, UNION ALL(allow duplicates), TRY_PARSE, TRY_CONVERT, TRY_CAST
